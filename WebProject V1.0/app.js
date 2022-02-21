44771bdcf53478d718e9a3b188ab7addf0399dfe
RedBiscuits <84057046+RedBiscuits@users.noreply.github.com>
2022-02-21 22:01:36 +0200

    Adding files
    files upload we didn't know Github back then

---
+++ WebProject V1.0/app.js
@@ -0,0 +1,6 @@
+﻿function contact() {
+    myWindow = window.open("ContactUs.html", "_blank", "width=500, height=700 , resize:none");
+}
+function FCISASU() {
+    myWindow = window.open("https://www.facebook.com/%D8%A7%D9%84%D8%B5%D9%81%D8%AD%D8%A9%D8%A7%D9%84%D8%B1%D8%B3%D9%85%D9%8A%D8%A9-%D9%84%D9%83%D9%84%D9%8A%D8%A9-%D8%A7%D9%84%D8%AD%D8%A7%D8%B3%D8%A8%D8%A7%D8%AA-%D9%88%D8%A7%D9%84%D9%85%D8%B9%D9%84%D9%88%D9%85%D8%A7%D8%AA-%D8%AC%D8%A7%D9%85%D8%B9%D8%A9-%D8%B9%D9%8A%D9%86-%D8%B4%D9%85%D8%B3-1839142163059502/", "_blank", "width=800, height=700 , resize:none");
+}
